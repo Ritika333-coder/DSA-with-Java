b3354092d055fdef6318539e02fda376ab5e99b6
Ritika <sb8813128@gmail.com>
2026-02-15 23:24:42 +0530

    Maximum wealth

---
+++ Linear_Search/MaxWealth.java
@@ -0,0 +1,24 @@
+package Linear_Search;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+        int[][] accounts ={
+            {1,2,3},
+            {3,2,1}
+        };
+        System.out.println(MaximumWealth(accounts));
+    }
+   public static int MaximumWealth(int[][]accounts){
+        int max=Integer.MIN_VALUE;
+        for(int row=0;row<accounts.length;row++){
+            int sum=0;
+            for(int col=0;col<accounts[row].length;col++){
+                sum+= accounts[row][col];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+    }
+}
